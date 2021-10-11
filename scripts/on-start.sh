759ba47fe11541a0264fc7854eb619a6fd6cccd6
prabu <prabuarjunan@gmail.com>
2021-10-11 12:33:16 -0700

    commit the script with the changes

--- scripts/on-start.sh
+++ scripts/on-start.sh
@@ -11,19 +11,21 @@ set -e
 #   2. The FSx for NetApp Ontap file system is accessible from the Notebook Instance
 #       - The Notebook Instance has to be created on the same VPN as the FSx for NetApp Ontap file system
 #       - The subnets and security groups have to be properly set up
-#   3. Set the FSX_DNS_NAME parameter below to the DNS name of the FSx for NetApp Ontap file system.
-#   4. Set the FSX_MOUNT_NAME parameter below to the Mount name of the FSx for NetApp Ontap file system.
+#   3. Set the FSXo_DNS_NAME parameter below to the DNS name of the FSx for NetApp Ontap file system.
+#   4. Set the FSXo_MOUNT_NAME parameter below to the Mount name of the FSx for NetApp Ontap file system.
 
 # PARAMETERS
 FSXo_DNS_NAME=IPaddress/DNS
 FSXo_MOUNT_NAME=fsx
+SVM_NAME=svm-0123456
+FSXo_ID=fs-0123456
 
 # First, we need to install the ontap-client libraries
-sudo yum install -y ontap-client
 
 # Now we can create the mount point and mount the file system
 sudo mkdir /fsx
-sudo mount -t lustre -o noatime,flock $FSXo_DNS_NAME@tcp:/$FSXo_MOUNT_NAME /fsx
+sudo mount -t nfs FSXo_DNS_NAME:/vol1 /fsx
+
 
 # Let's make sure we have the appropriate access to the directory
 sudo chmod go+rw /fsx
